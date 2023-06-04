41f9488e100911c2e003acf6ede968062774bac0
alfauzan003 <habibalfauzan1@gmail.com>
2023-06-04 10:47:05 +0700

    Add nutrition needed, imageURL, dateRecord

--- routes/foods.js
+++ routes/foods.js
@@ -49,21 +49,23 @@ router.get('/data', auth.verifyLogin, async (req, res, next) => {
 router.post('/records/:uid', auth.verifyLogin, async (req, res, next) => {
   try {
     const foods = {
-      food: req.body.food
+      food: req.body.food,
+      imageURL: req.body.imageURL,
+      dateRecord: req.body.dateRecord
     }
     const food = foods.food
     const response = await axios.get(
       `https://api.nal.usda.gov/fdc/v1/foods/search?api_key=DEMO_KEY&query=${food}`
     )
-    const date = new Date()
     console.log(response.data)
     const dataFood = {
       nameFood: response.data.foods[1].description,
-      dateTime: date,
+      dateRecord: foods.dateRecord,
       protein: response.data.foods[1].foodNutrients[0].value,
       lipid: response.data.foods[1].foodNutrients[1].value,
       Carbohydrate: response.data.foods[1].foodNutrients[2].value,
-      Calory: response.data.foods[1].foodNutrients[3].value
+      Calory: response.data.foods[1].foodNutrients[3].value,
+      imageURL: foods.imageURL
     }
     await admin
       .firestore()
@@ -92,24 +94,22 @@ router.get('/records/:uid', auth.verifyLogin, function (req, res, next) {
       .collection('users')
       .doc(req.params.uid)
       .collection('records')
-      .orderBy('dateTime', 'desc')
+      .orderBy('dateRecord', 'desc')
       .limit(limit)
       .offset(offset)
       .get()
     snapshot.docs.forEach((doc) => {
       const finalData = {
-        reocrdId: doc.id,
+        recordId: doc.id,
         nameFood: doc.data().nameFood,
-        dateRecord: doc.data().dateTime,
+        dateRecord: doc.data().dateRecord,
         protein: doc.data().protein,
         calory: doc.data().Calory,
         lipid: doc.data().lipid,
-        carbohydrate: doc.data().Carbohydrate
+        carbohydrate: doc.data().Carbohydrate,
+        imageURL: doc.data().imageURL,
       }
       arrayJson.push(finalData)
-      console.log(doc.id)
-      const test = doc.data().dateTime
-      console.log(typeof test)
     })
     return arrayJson
   }
@@ -127,13 +127,15 @@ router.get('/records/:uid/:recordId', auth.verifyLogin, function (req, res, next
       .doc(req.params.recordId)
       .get()
     const finalData = {
-      reocrdId: snapshot.id,
+      recordId: snapshot.id,
       nameFood: snapshot.data().nameFood,
-      dateRecord: snapshot.data().dateTime,
+      dateRecord: snapshot.data().dateRecord,
       protein: snapshot.data().protein,
       calory: snapshot.data().Calory,
       lipid: snapshot.data().lipid,
-      carbohydrate: snapshot.data().Carbohydrate
+      carbohydrate: snapshot.data().Carbohydrate,
+      imageURL: snapshot.data().imageURL,
+      dateRecord: snapshot.data().dateRecord
     }
     return finalData
   }
--- routes/users.js
+++ routes/users.js
@@ -16,15 +16,96 @@ router.get('/', auth.verifyLogin, function (req, res, next) {
 // Create Document ID same as firebase auth UID
 router.post('/register', async (req, res, next) => {
   async function createUser() {
+    const today = new Date()
     const date = new Date(req.body.birthDate)
+    const age = today.getFullYear() - date.getFullYear()
+    console.log(age)
+    const gender = req.body.gender
+    const weight = req.body.weight
+    const height = req.body.height
+    if(gender=='male' || 'laki-laki' || 'pria'){
+      if(age >= 10 && age <= 12){
+        var caloryNeed = 2000
+        var proteinNeed = 50
+        var fatNeed = 65
+      } else if(age >= 13 && age <= 15){
+        var caloryNeed = 2400
+        var proteinNeed = 70
+        var fatNeed = 80
+      } else if(age >= 16 && age <= 18){
+        var caloryNeed = 2650
+        var proteinNeed = 75
+        var fatNeed = 85
+      } else if(age >= 19 && age <= 29){
+        var caloryNeed = 2650
+        var proteinNeed = 65
+        var fatNeed = 75
+      } else if(age >= 30 && age <= 49){
+        var caloryNeed = 2550
+        var proteinNeed = 65
+        var fatNeed = 70
+      } else if(age >= 50 && age <= 64){
+        var caloryNeed = 2150
+        var proteinNeed =65
+        var fatNeed = 60
+      } else if(age >= 65 && age <= 80){
+        var caloryNeed = 1800
+        var proteinNeed = 64
+        var fatNeed = 50
+      } else if(age > 80){
+        var caloryNeed = 1600
+        var proteinNeed = 64
+        var fatNeed = 45
+      } 
+    } else if (gender=='female' || 'perempuan' || 'wanita'){
+      if(age >= 10 && age <= 12){
+        var caloryNeed = 1900
+        var proteinNeed = 55
+        var fatNeed = 65
+      } else if(age >= 13 && age <= 15){
+        var caloryNeed = 2050
+        var proteinNeed = 65
+        var fatNeed = 70
+      } else if(age >= 16 && age <= 18){
+        var caloryNeed = 2100
+        var proteinNeed = 65
+        var fatNeed = 70
+      } else if(age >= 19 && age <= 29){
+        var caloryNeed = 2250
+        var proteinNeed = 60
+        var fatNeed = 65
+      } else if(age >= 30 && age <= 49){
+        var caloryNeed = 2150
+        var proteinNeed = 60
+        var fatNeed = 60
+      } else if(age >= 50 && age <= 64){
+        var caloryNeed = 1800
+        var proteinNeed =60
+        var fatNeed = 50
+      } else if(age >= 65 && age <= 80){
+        var caloryNeed = 1550
+        var proteinNeed = 58
+        var fatNeed = 45
+      } else if(age > 80){
+        var caloryNeed = 1400
+        var proteinNeed = 58
+        var fatNeed = 40
+      }
+    }
+
     const user = {
       uid: req.body.uid,
       name: req.body.name,
       email: req.body.email,
+      gender: gender,
       birthDate: date,
-      height: req.body.height,
-      weight: req.body.weight,
+      height: height,
+      weight: weight,
+      caloryNeed: caloryNeed,
+      proteinNeed: proteinNeed,
+      fatNeed: fatNeed,
     }
+    
     await admin
       .firestore()
       .collection('users')
@@ -55,6 +136,9 @@ router.get('/:uid', auth.verifyLogin, async (req, res, next) => {
         birthDate: data.data().birthDate,
         height: data.data().height,
         weight: data.data().weight,
+        caloryNeed: caloryNeed,
+        proteinNeed: proteinNeed,
+        fatNeed: fatNeed,
       }
       arrayProfile.push(finalProfile)
     })
